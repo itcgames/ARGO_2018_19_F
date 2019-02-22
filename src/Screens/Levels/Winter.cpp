6c0b99686850f55192381dfade20f7ca49f7e684
DJ O'Leary <djoleary39@ymail.com>
2019-02-22 11:16:05 +0000

    Added winter

--- src/Screens/Levels/Winter.cpp
+++ src/Screens/Levels/Winter.cpp
@@ -4,9 +4,15 @@
 /// 
 /// </summary>
 Winter::Winter(ScreenManager* screenManager, SDL_Renderer* renderer) :
-	Screen(screenManager, renderer)
+	Level(screenManager, renderer, Vector(), Vector())
 {
 	m_screenID = "Play";
+	m_previousScreenID = "ModeSelect";
+
+	m_entityManager.createBackground(SDL2Help::LoadTexture(m_resourcesPath + "Backgrounds/Winter.png", m_renderer), { 0, 0, 6708, 3805 });
+
+	spawnPlayers(1, false);
+	spawnLevelObstacles();
 }
 
 
@@ -15,8 +21,9 @@ Winter::Winter(ScreenManager* screenManager, SDL_Renderer* renderer) :
 /// 
 /// </summary>
 /// <param name="dt"></param>
-void Winter::update(double dt, SDL_Event& e)
+void Winter::update(double dt)
 {
+	m_entityManager.update(dt);
 }
 
 
@@ -27,4 +34,37 @@ void Winter::update(double dt, SDL_Event& e)
 /// <param name="renderer"></param>
 void Winter::render()
 {
+	m_entityManager.render();
+}
+
+
+
+/// <summary>
+/// 
+/// </summary>
+void Winter::spawnLevelObstacles()
+{
+	m_entityManager.createPlatform(Vector(0, 500), SDL2Help::LoadTexture(m_resourcesPath + "Missing.png", m_renderer), { 0, 0, 200, 200 }, {0, 0, 200, 400}, {0, 0, 200, 400});
+	m_entityManager.createPlatform(Vector(1400, 200), SDL2Help::LoadTexture(m_resourcesPath + "Missing.png", m_renderer), { 0, 0, 200, 200 }, { 0, 0, 200, 700 }, { 0, 0, 200, 700 });
+}
+
+
+
+/// <summary>
+/// 
+/// </summary>
+void Winter::loadTextures()
+{
+	//	Red
+	m_playerTextures["Red"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Santa//Red_Santa.png", m_renderer));
+	m_playerTextures["Red"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Snowman//Red_Snowman.png", m_renderer));
+	//	Blue
+	m_playerTextures["Blue"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Santa//Blue_Santa.png", m_renderer));
+	m_playerTextures["Blue"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Snowman//Blue_Snowman.png", m_renderer));
+	//	Green
+	m_playerTextures["Green"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Santa//Green_Santa.png", m_renderer));
+	m_playerTextures["Green"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Snowman//Green_Snowman.png", m_renderer));
+	//	Yellow
+	m_playerTextures["Yellow"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Santa//Yellow_Santa.png", m_renderer));
+	m_playerTextures["Yellow"].push_back(SDL2Help::LoadTexture(m_resourcesPath + "Characters//Winter//Snowman//Yellow_Snowman.png", m_renderer));
 }
